edb76823e1fb493b4b327f03f7d6e31e132b38a9
humhliao <humhliao@gmail.com>
2024-10-11 20:53:15 +0700

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -79,6 +79,13 @@ document.querySelector('.js-products-grid')
           });
         }
 
-        console.log(cart);
+        let cartQuantity = 0;
+
+        cart.forEach((item) => {
+          cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity')
+          .innerHTML = cartQuantity;
       });
     });
